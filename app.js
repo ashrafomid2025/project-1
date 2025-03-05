5ce4873db2be6bf9990175bffc0c8e4fac2a445f
ashrafomid2025 <omidmashraf@gmail.com>
2025-03-05 13:30:11 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const family = document.getElementById("family");
+const fr = document.getElementById("friends");
+const n = document.getElementById("nature");
+const n1 = document.getElementsByClassName('n');
+n.addEventListener('click',()=>{
+    // n1.style.display ="block";
+    n1.classList.add('b');
+})
